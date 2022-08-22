aaf853a77927ddc210fc6026e0e414ff40a6b07c
Saphira <saphira.xavier@pagar.me>
2022-08-22 15:56:43 -0300

    ✅ update

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ const navLinks = document.querySelector('.nav-links')
 menuBtn.addEventListener('click',()=>{
     navLinks.classList.toggle('mobile-menu')
 });
+
+
+const body = document.querySelector('body')
+const headerMenu = document.querySelector('.header-menu')
+console.log(body)
+
+body.addEventListener('scroll',()=>{
+    headerMenu.classList.add('header-menu-fixed')
+});
